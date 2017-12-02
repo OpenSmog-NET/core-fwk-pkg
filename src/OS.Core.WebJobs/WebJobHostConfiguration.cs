d3564ff1f2b1d61a24adf52bb672fba716614ec1
Maciej Misztal <mmisztal1980@gmail.com>
2017-12-02 16:01:01 +0100

    added connection string validation

--- src/OS.Core.WebJobs/WebJobHostConfiguration.cs
+++ src/OS.Core.WebJobs/WebJobHostConfiguration.cs
@@ -29,11 +29,16 @@ namespace OS.Core.WebJobs
 
         private static JobHostConfiguration CreateJobHostConfiguration(IServiceProvider container, WebJobSettings settings)
         {
+            if (string.IsNullOrEmpty(settings.StorageConnectionString)) throw new ArgumentException($"Connection string : {settings.StorageConnectionStringName} is missing or emtpy");
+            if (string.IsNullOrEmpty(settings.DashboardConnectionString)) throw new ArgumentException($"Connection string : {settings.DashboardConnectionStringName} is missing or emtpy");
+
             Environment.SetEnvironmentVariable("AzureWebJobsStorage", settings.StorageConnectionString);
             Environment.SetEnvironmentVariable("AzureWebJobsDashboard", settings.DashboardConnectionString);
 
             var jobHostConfiguration = new JobHostConfiguration()
             {
+                DashboardConnectionString = settings.DashboardConnectionString,
+                StorageConnectionString = settings.StorageConnectionString,
                 JobActivator = new WebJobActivator(container)
             };
 
